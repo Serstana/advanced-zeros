bdb05128789c904ee0a1fecb8caeed0b0d42a247
Anastasiya Staravoitava <vampireslayer@mai.ru>
2019-03-18 20:08:09 +0300

    feat: advanced-zeros function is ready

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
+let n=base;
+let k=0;
+let prohod=0;
+let min=0;
+
+for(let i = base; i > 1; --i){
+  if(base % i == 0){
+    let j = i - 1;
+    for(;j > 1; --j){
+      if(i % j == 0){
+        break;
+      }
+    }
+    if(j == 1){
+      n = i;
+      let l = n;
+      k=0;
+      while(number / l >= 1){
+        k += Math.floor(number / l);
+        l *= n;
+      }
+      l = 0;
+      while(base % n == 0){
+        ++l;
+        base /= n;
+      }
+      k=Math.floor(k/l);
+      if(prohod==0){
+        min = k;
+      }else if (min>k){
+        min=k;
+      } 
+      prohod++;
+    }
+  }
+} return min
 }
